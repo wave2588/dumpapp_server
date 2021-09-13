c84d022eb73bbe29db2dba14a513067c6ea3bb33
wave <15711367321@163.com>
2021-09-13 22:48:43 +0800

    🎲

--- cmd/cron/conclusion/conclusion.go
+++ cmd/cron/conclusion/conclusion.go
@@ -69,18 +69,60 @@ func run() {
 		}
 	}
 
+	/// 计算使用人数
+	numbers := getMemberDownloadMap(ctx, startAt)
+	downloadedCount := 0
+	downloadedMember := make(map[int64]struct{}, 0)
+	for _, number := range numbers {
+		if number.Status == enum.MemberDownloadNumberStatusUsed {
+			downloadedCount++
+			downloadedMember[number.MemberID] = struct{}{}
+		}
+	}
+
 	contentStr := fmt.Sprintf("<font color=\"info\">每日总结\n截止昨日此时数据统计如下：</font>\n>")
 	newMemberStr := fmt.Sprintf("新注册用户：<font color=\"comment\">%d</font> 人\n", len(memberIDs))
 	paidMemberStr := fmt.Sprintf("新用户付费率：：<font color=\"comment\">%.2f%%</font>\n", cast.ToFloat64(len(paidMemberIDs))/cast.ToFloat64(len(memberIDs))*100)
 	orderCountStr := fmt.Sprintf("总订单：<font color=\"comment\">%d</font>\n", orderCount)
 	amountStr := fmt.Sprintf("总收入：<font color=\"comment\">%v</font>\n", amount)
+	downloadedStr := fmt.Sprintf("使用次数：<font color=\"comment\">%d</font>\n", downloadedCount)
+	downloadedMemberStr := fmt.Sprintf("下载人数：<font color=\"comment\">%d</font>\n", len(downloadedMember))
 	timeStr := fmt.Sprintf("发送时间：<font color=\"comment\">%s</font>\n", now.Format("2006-01-02 15:04:05"))
 	data := map[string]interface{}{
 		"msgtype": "markdown",
 		"markdown": map[string]interface{}{
 			"content": contentStr +
-				newMemberStr + paidMemberStr + orderCountStr + amountStr + timeStr,
+				newMemberStr + paidMemberStr + orderCountStr + amountStr + downloadedStr + downloadedMemberStr + timeStr,
 		},
 	}
 	util2.SendWeiXinBot(ctx, config.DumpConfig.AppConfig.TencentGroupKey, data, []string{"@all"})
+}
+
+func getMemberDownloadMap(ctx context.Context, startAt time.Time) []*models.MemberDownloadNumber {
+	offset := 0
+	bulkSize := 100
+	hasNext := true
+
+	result := make([]*models.MemberDownloadNumber, 0)
+	for hasNext {
+		fmt.Println(fmt.Sprintf("offset: %d...", offset))
+
+		filter := []qm.QueryMod{
+			models.MemberDownloadNumberWhere.CreatedAt.GT(startAt),
+		}
+		ids, err := impl.DefaultMemberDownloadNumberDAO.ListIDs(ctx, offset, bulkSize, filter, nil)
+		util.PanicIf(err)
+
+		data, err := impl.DefaultMemberDownloadNumberDAO.BatchGet(ctx, ids)
+		util.PanicIf(err)
+
+		hasNext = len(ids) >= bulkSize
+		offset += len(ids)
+
+		for _, number := range data {
+			result = append(result, number)
+		}
+	}
+
+	return result
 }
--- cmd/cron/delete_ipa/delete_interim_ipa.go
+++ cmd/cron/delete_ipa/delete_interim_ipa.go
@@ -2,10 +2,14 @@ package delete_ipa
 
 import (
 	"context"
+	"dumpapp_server/pkg/common/clients"
+	"dumpapp_server/pkg/common/constant"
 	"dumpapp_server/pkg/common/util"
+	"dumpapp_server/pkg/config"
 	impl2 "dumpapp_server/pkg/controller/impl"
 	"dumpapp_server/pkg/dao/impl"
 	"dumpapp_server/pkg/dao/models"
+	util2 "dumpapp_server/pkg/util"
 	"fmt"
 	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 	"time"
@@ -14,6 +18,12 @@ import (
 func Run() {
 	fmt.Println("DeleteInterimIpa")
 	run()
+}
+
+type deleteIpa struct {
+	ID      int64
+	Name    string
+	Version string
 }
 
 func run() {
@@ -25,6 +35,12 @@ func run() {
 
 	tm := time.Now().AddDate(0, 0, -3)
 
+	txn := clients.GetMySQLTransaction(ctx, clients.MySQLConnectionsPool, true)
+	defer clients.MustClearMySQLTransaction(ctx, txn)
+	ctx = context.WithValue(ctx, constant.TransactionKeyTxn, txn)
+
+	deleteIpaMap := make(map[int64][]*deleteIpa, 0)
+
 	for hasNext {
 		fmt.Println(fmt.Sprintf("offset: %d...", offset))
 
@@ -35,22 +51,54 @@ func run() {
 		ids, err := impl.DefaultIpaDAO.ListIDs(ctx, offset, bulkSize, filters, nil)
 		util.PanicIf(err)
 
+		ipaMap, err := impl.DefaultIpaDAO.BatchGet(ctx, ids)
+		util.PanicIf(err)
+
 		hasNext = len(ids) >= bulkSize
 		offset += len(ids)
 
-		fmt.Println(ids)
-
-		for _, id := range ids {
-			util.PanicIf(impl.DefaultIpaDAO.Delete(ctx, id))
+		for _, ipa := range ipaMap {
+			util.PanicIf(impl.DefaultIpaDAO.Delete(ctx, ipa.ID))
 		}
 		ipaVersions, err := impl.DefaultIpaVersionDAO.BatchGetIpaVersions(ctx, ids)
 		util.PanicIf(err)
 
-		for _, versions := range ipaVersions {
+		for ipaID, versions := range ipaVersions {
+			ipa, ok := ipaMap[ipaID]
+			if !ok {
+				continue
+			}
 			for _, version := range versions {
 				util.PanicIf(impl.DefaultIpaVersionDAO.Delete(ctx, version.ID))
 				util.PanicIf(impl2.DefaultTencentController.DeleteFile(ctx, version.TokenPath))
+				deleteIpaMap[ipaID] = append(deleteIpaMap[ipaID], &deleteIpa{
+					ID:      ipaID,
+					Name:    ipa.Name,
+					Version: version.Version,
+				})
 			}
 		}
 	}
+
+	clients.MustCommit(ctx, txn)
+	util.ResetCtxKey(ctx, constant.TransactionKeyTxn)
+
+	nameStr := ""
+	for _, is := range deleteIpaMap {
+		for _, ipa := range is {
+			nameStr += fmt.Sprintf("<font color=\"info\">名称：</font>%s %s\n>", ipa.Name, ipa.Version)
+		}
+	}
+
+	contentStr := fmt.Sprintf("<font color=\"warning\">已删除临时 ipa：</font>\n>")
+	deleteIpaStr := fmt.Sprintf("<font color=\"comment\">%s</font>", nameStr)
+	data := map[string]interface{}{
+		"msgtype": "markdown",
+		"markdown": map[string]interface{}{
+			"content": contentStr +
+				deleteIpaStr,
+		},
+	}
+	util2.SendWeiXinBot(ctx, config.DumpConfig.AppConfig.TencentGroupKey, data, []string{"@all"})
+
 }
