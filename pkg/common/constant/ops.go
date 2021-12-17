4cf7dd6d70f0f5a7ac45416d5a38cc52f2802eca
wave <15711367321@163.com>
2021-12-18 01:05:40 +0800

    😂

--- pkg/common/constant/ops.go
+++ pkg/common/constant/ops.go
@@ -1,10 +1,10 @@
 package constant
 
 var OpsAuthNameMap = map[string]int64{
-	"admin": 1390315185599680512,
+	"x-auth-header": 1390315185599680512,
 }
 
 var OpsAuthMemberIDMap = map[int64]string{
-	1390315185599680512: "admin",
-	1431956649500741632: "admin",
+	1390315185599680512: "x-auth-header",
+	1431956649500741632: "x-auth-header",
 }
