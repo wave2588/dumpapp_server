954061d0f378cd448fce6acd351d4ce67504ce63
wave <15711367321@163.com>
2021-12-20 23:31:39 +0800

    😌admin

--- pkg/common/constant/ops.go
+++ pkg/common/constant/ops.go
@@ -7,4 +7,5 @@ var OpsAuthNameMap = map[string]int64{
 var OpsAuthMemberIDMap = map[int64]string{
 	1390315185599680512: "x-auth-header",
 	1431956649500741632: "x-auth-header",
+	1399256949626769408: "x-auth-header",
 }
