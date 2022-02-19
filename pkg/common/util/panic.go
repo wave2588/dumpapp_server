ed6f855d3ee8db8396ff90455a0fffc68e11fbc7
wave <15711367321@163.com>
2022-02-19 14:03:41 +0800

    fix

--- pkg/common/util/panic.go
+++ pkg/common/util/panic.go
@@ -2,10 +2,14 @@ package util
 
 import (
 	"github.com/pkg/errors"
+	"io"
 )
 
 func PanicIf(err error) {
 	if err != nil {
+		if err == io.EOF {
+			return
+		}
 		e := errors.WithStack(err)
 		panic(e)
 	}
