4e5ac6d648924ee5fde7d70a0f741839ad1684fd
zhanghaibo <zhanghaibo@zhihu.com>
2022-04-08 17:36:37 +0800

    pv

--- pkg/dao/impl/statistics.go
+++ pkg/dao/impl/statistics.go
@@ -3,7 +3,6 @@ package impl
 import (
 	"context"
 	"dumpapp_server/pkg/common/clients"
-	"encoding/json"
 	"fmt"
 	"github.com/go-redis/redis/v8"
 	"github.com/spf13/cast"
@@ -40,18 +39,13 @@ func (d *StatisticsDAO) AddStatistics(ctx context.Context, memberID int64) error
 
 func (d *StatisticsDAO) GetPageView(ctx context.Context, time time.Time) (int64, error) {
 	key := d.generateStatisticsKey(time)
-	data, err := d.redis.ZRevRange(ctx, key, 0, 1000).Result()
+	data, err := d.redis.ZRevRangeWithScores(ctx, key, 0, -1).Result()
 	if err != nil {
 		return 0, err
 	}
 	var count int64 = 0
 	for _, datum := range data {
-		var item redis.Z
-		err = json.Unmarshal([]byte(datum), &item)
-		if err != nil {
-			return 0, err
-		}
-		count += cast.ToInt64(item.Score)
+		count += cast.ToInt64(datum.Score)
 	}
 	return count, nil
 }
