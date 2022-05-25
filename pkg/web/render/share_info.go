315c37a4bc489e986a40f35ae417fb94c15cc368
zhanghaibo <zhanghaibo@zhihu.com>
2022-05-25 13:46:06 +0800

    wechat

--- pkg/web/render/share_info.go
+++ pkg/web/render/share_info.go
@@ -4,6 +4,7 @@ type ShareInfo struct {
 	QQGroupURL  string `json:"qq_group_url"`
 	QQGroupNum  int64  `json:"qq_group_num"`
 	TelegramURL string `json:"telegram_url"`
+	WechatURL   string `json:"wechat_url"`
 	Content     string `json:"content"`
 }
 
@@ -13,5 +14,6 @@ func MustRenderShareInfo() *ShareInfo {
 		QQGroupURL:  "https://jq.qq.com/?_wv=1027&k=jPKZ0Uk7",
 		QQGroupNum:  1137936037,
 		TelegramURL: "https://t.me/+VGGU8RYVqDo1NTg1",
+		WechatURL:   "https://work.weixin.qq.com/u/vc3a10ae3518beb870?v=3.1.23.79300&src=wx",
 	}
 }
