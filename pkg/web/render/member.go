3cec95cd1153c95306305622382bc5049a413523
zhanghaibo <zhanghaibo@zhihu.com>
2022-10-23 19:18:45 +0800

    fix

--- pkg/web/render/member.go
+++ pkg/web/render/member.go
@@ -183,7 +183,7 @@ func (f *MemberRender) fetch(ctx context.Context) {
 			CreatedAt: account.CreatedAt.Unix(),
 			UpdatedAt: account.UpdatedAt.Unix(),
 			PayCampaign: &PayCampaign{
-				Description: "网站充值优惠大放送，充500 送 15 ，冲1000送70，冲2000 送 260 ，冲5000 送 1290，冲 10000 送 3330。",
+				Description: "网站充值优惠大放送，充500 送 15 ，冲1000送70，冲2000 送 260 ，冲5000 送 1290，冲 10000 送 3330。(如遇充值失败请联系管理员)",
 			},
 		}
 	}
